d6291c621ba88721ee2327472e51fad0e75392f3
ZeeshanAhmadsi <155843561+ZeeshanAhmadsi@users.noreply.github.com>
2026-01-08 23:13:44 +0530

    implemented problem-detail-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useUser } from '@clerk/clerk-react';
 import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './Pages/HomePage';
 import ProblemsPage from './Pages/ProblemsPage';
+import ProblemPage from './Pages/problemPage';
 import {Toaster} from 'react-hot-toast';
 import DashboardPage from './Pages/DashboardPage';
 
@@ -16,6 +17,7 @@ function App() {
       <Route path="/" element={!isSignedIn ? <HomePage/> : <Navigate to={"/dashboard"}/>}/>
       <Route path="/dashboard" element={isSignedIn ? <DashboardPage/> : <Navigate to={"/"}/>}/>
       <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
+      <Route path="/problem/:id" element = {isSignedIn ? <ProblemPage/> : <Navigate to={"/"}/>}/>
     </Routes>
     <Toaster toastOptions={{duration:3000}}/>
     </>
